0da6419a42a9c8ee3668ef3772a68b47d4c418a2
alisadeghi <alisadghi228@gmail.com>
2024-12-09 12:15:19 +0330

    fix : removing sharp from package json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
-"use client";
-import Body from "@/components/layout/body/Body";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "animate.css";
+// "use client";
+// import Body from "@/components/layout/body/Body";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "animate.css";
 // import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 // import { useEffect } from "react";
 export default function Home() {
   // useEffect(() => {
   //   AOS.init();
   // }, []);
   return (
-    <div className="home-layout">
-      <div className="home-response">
-        {/* <Body /> */}
-        <h1>Body</h1>
-      </div>
-    </div>
+    <>
+      <h1>Body</h1>
+    </>
   );
 }
