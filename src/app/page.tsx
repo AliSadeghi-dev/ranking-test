9731fc347722dd7ffdfc88c5ac036fb0d4776cb3
alisadeghi <alisadghi228@gmail.com>
2024-12-09 12:01:14 +0330

    fix : remove useEffect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,13 @@
 import Body from "@/components/layout/body/Body";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "animate.css";
-// @ts-expect-error This type is incompatible due to library limitations.
-import AOS from "aos";
+// import AOS from "aos";
 import "aos/dist/aos.css";
-import { useEffect } from "react";
+// import { useEffect } from "react";
 export default function Home() {
-  useEffect(() => {
-    AOS.init();
-  }, []);
+  // useEffect(() => {
+  //   AOS.init();
+  // }, []);
   return (
     <div className="home-layout">
       <div className="home-response">
